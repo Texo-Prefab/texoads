6dc3fe8670c022f4073caae459265c664f8c518a
AbdulBaseer-01 <mohammadabdulbaseer8@gmail.com>
2026-01-28 17:36:58 +0530

    sitemap updated

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,27 +1,18 @@
-// app/sitemap.tsx
-import { NextResponse } from 'next/server';
+import { MetadataRoute } from 'next';
 
-export async function GET() {
-  const baseUrl = 'https://texoprefabworld.in';
-
-  const pages = ['', 'farmhouses', 'offices'];
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages
-  .map((page) => {
-    return `
-  <url>
-    <loc>${baseUrl}/${page}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>${page === '' ? '1.0' : '0.8'}</priority>
-  </url>
-  `;
-  })
-  .join('')}
-</urlset>`;
-
-  return new NextResponse(sitemap, {
-    headers: { 'Content-Type': 'application/xml' },
-  });
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://texoprefabworld.in/farmhouses',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://texoprefabworld.in/prefaboffices',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+  ];
 }
