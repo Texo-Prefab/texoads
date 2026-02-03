226814502b61d697d895e88fd811fbed713b83c7
AbdulBaseer-01 <mohammadabdulbaseer8@gmail.com>
2026-02-03 17:08:37 +0530

    google tags gicchi

--- app/farmhouses/layout.tsx
+++ app/farmhouses/layout.tsx
@@ -1,4 +1,3 @@
-import Script from "next/script";
 
 export default function FarmhousesLayout({
   children,
@@ -7,20 +6,6 @@ export default function FarmhousesLayout({
 }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-70JJF01Q2Q"
-        strategy="afterInteractive"
-      />
-
-      <Script id="ga-prefaboffices" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-70JJF01Q2Q');
-        `}
-      </Script>
-
       {children}
     </>
   );
--- app/prefaboffices/layout.tsx
+++ app/prefaboffices/layout.tsx
@@ -1,4 +1,4 @@
-import Script from "next/script";
+
 
 export default function PrefabOfficesLayout({
   children,
@@ -7,20 +7,6 @@ export default function PrefabOfficesLayout({
 }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-70JJF01Q2Q"
-        strategy="afterInteractive"
-      />
-
-      <Script id="ga-prefaboffices" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-70JJF01Q2Q');
-        `}
-      </Script>
-
       {children}
     </>
   );
